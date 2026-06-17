b7323709dbae1cfde04a211e994a3bbed0519602
yassine Bessaim <yassinebessaim@gmail.com>
2026-06-17 11:24:11 +0200

    ajout commentaire explicatif connexion bdd

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+#importation de l'objet cnx dans main.py
 from ConnexionBdd import cnx
+
+#connexion bdd
 bdd = cnx
+
 print("connexion réussi !")
 
+#création d'une requête pour tester la connexion
 cur = cnx.cursor()
 query = "select * from appareil"
 
+#éxecution de la requête
 cur.execute(query)
 result = cur.fetchall()
 for f in result:
